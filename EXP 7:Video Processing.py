798e1456b2f508600743380eefd793d557a70c72
192572099simats-ux <192572099.simats@saveetha.com>
2026-07-24 14:26:57 +0530

    Create EXP 7:Video Processing.py

---
+++ EXP 7:Video Processing.py
@@ -0,0 +1,13 @@
+import cv2
+import time
+cap = cv2.VideoCapture("video.mp4")
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow("Video", frame)
+    time.sleep(0.1)      # Slow Motion
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+cap.release()
+cv2.destroyAllWindows()
