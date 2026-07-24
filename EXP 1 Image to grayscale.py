45620eff3a790e983c955a47d23df0ad49d4543e
192572099simats-ux <192572099.simats@saveetha.com>
2026-07-24 14:18:25 +0530

    Update and rename EXP 1 Image to grayscale to EXP 1 Image to grayscale.py

--- EXP 1 Image to grayscale.py
+++ EXP 1 Image to grayscale.py
@@ -1,21 +1,16 @@
 import cv2
-
 # Read the image
 image = cv2.imread("image.jpg")
-
 # Check if the image is loaded
 if image is None:
     print("Error: Image not found!")
 else:
     # Convert to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     # Display images
     cv2.imshow("Original Image", image)
     cv2.imshow("Gray-scale Image", gray_image)
-
     # Save the grayscale image
     cv2.imwrite("gray_image.jpg", gray_image)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
