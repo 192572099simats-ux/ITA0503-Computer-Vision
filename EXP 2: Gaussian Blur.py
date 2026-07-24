8c081ea0f74a19299e0449467b6275886efde998
192572099simats-ux <192572099.simats@saveetha.com>
2026-07-24 14:15:19 +0530

    Create EXP 2: Gaussian Blur.py

---
+++ EXP 2: Gaussian Blur.py
@@ -0,0 +1,7 @@
+import cv2
+image = cv2.imread("image.jpg")
+blur = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow("Original Image", image)
+cv2.imshow("Gaussian Blur", blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
