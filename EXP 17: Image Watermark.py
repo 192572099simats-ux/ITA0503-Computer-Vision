0147aa992721104b954cee3982467974541f830b
192572099simats-ux <192572099.simats@saveetha.com>
2026-07-24 14:42:40 +0530

    Create EXP 17: Image Watermark.py

---
+++ EXP 17: Image Watermark.py
@@ -0,0 +1,7 @@
+import cv2
+image = cv2.imread("image.jpg")
+cv2.putText(image,"WATERMARK",(50,50),
+cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+cv2.imshow("Watermark", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
